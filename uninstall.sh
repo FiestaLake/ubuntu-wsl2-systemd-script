95a6bbab2e7296dfec9a9a852f75cb1a94a8d115
Sung Mingi <FiestaLake@protonmail.com>
2021-03-29 12:37:22 +0900

    Update uninstall.sh
    Signed-off-by: Sung Mingi <FiestaLake@protonmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-if [ "$(id -u)" != "0" ]; then
-  exec sudo "$0" "$1"
-fi
 
 cd2=$(pwd)
-#sudo apt update -yqqqq
 echo "Uninstalling ubuntu-wsl2-systemd-hack"
 sudo rm -f /usr/sbin/start-systemd-namespace
 sudo rm -f /usr/sbin/enter-systemd-namespace
 cd /var/tmp
-sudo grep -v "source /usr/sbin/start-systemd-namespace" /etc/bash.bashrc > tmpfile && mv tmpfile /etc/bash.bashrc
+sudo grep -v "source /usr/sbin/start-systemd-namespace" /etc/bash.bashrc > tmpfile && sudo mv tmpfile /etc/bash.bashrc
 cd $cd2

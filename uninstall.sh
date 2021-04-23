bf77fb8d1e005eb03c90c0fab07cb207bf19b004
Smg <FiestaLake@protonmail.com>
2021-04-23 15:05:57 +0900

    uninstall.sh: Improve removing the script
    Signed-off-by: Sung Mingi <FiestaLake@protonmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-cd2=$(pwd)
-echo "Uninstalling ubuntu-wsl2-systemd-hack"
-sudo rm -f /usr/sbin/start-systemd-namespace
-sudo rm -f /usr/sbin/enter-systemd-namespace
+self_dir=$(pwd)
+
+echo "Uninstalling ubuntu-wsl2-systemd-script"
+
+sudo rm -rf /etc/fish/conf.d/start-systemd-namespace.fish
+sudo rm -rf /usr/sbin/start-systemd-namespace
+sudo rm -rf /usr/sbin/enter-systemd-namespace
+sudo rm -rf /etc/sudoers.d/systemd-namespace
+
 cd /var/tmp
-sudo grep -v "source /usr/sbin/start-systemd-namespace" /etc/bash.bashrc > tmpfile && sudo mv tmpfile /etc/bash.bashrc
-cd $cd2
+# Try bash first.
+sudo grep -v "# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc > tmpfile
+sudo mv tmpfile /etc/bash.bashrc
+# Then try zsh.
+if [ -f "/etc/zsh/zshrc" ]; then
+sudo grep -v "# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/zsh/zshrc > tmpfile
+sudo mv tmpfile /etc/zsh/zshrc
+fi
+
+cd $self_dir

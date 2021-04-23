0db37747487901a6581f3f72e55e5e0d556423b5
Sung Mingi <FiestaLake@protonmail.com>
2021-04-23 15:55:56 +0900

    uninstall.sh: Improve removing lines in bashrc or zshrc
    Signed-off-by: Sung Mingi <FiestaLake@protonmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -11,11 +11,13 @@ sudo rm -rf /etc/sudoers.d/systemd-namespace
 
 cd /var/tmp
 # Try bash first.
-sudo grep -v "# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc > tmpfile
+sudo grep -v "# Start or enter a PID namespace in WSL2
+source /usr/sbin/start-systemd-namespace" /etc/bash.bashrc > tmpfile
 sudo mv tmpfile /etc/bash.bashrc
 # Then try zsh.
 if [ -f "/etc/zsh/zshrc" ]; then
-sudo grep -v "# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/zsh/zshrc > tmpfile
+sudo grep -v "# Start or enter a PID namespace in WSL2
+source /usr/sbin/start-systemd-namespace" /etc/zsh/zshrc > tmpfile
 sudo mv tmpfile /etc/zsh/zshrc
 fi
 
